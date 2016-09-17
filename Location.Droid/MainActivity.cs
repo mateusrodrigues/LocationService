9525b4a2fa40492cf9d5230635d2edaf6ca1ef84
Mateus Rodrigues de Morais <mateus.rodrigues@live.com>
2016-09-17 15:49:37 -0300

    [Mobile] Main Activity design and code implementation

--- Location.Droid/MainActivity.cs
+++ Location.Droid/MainActivity.cs
@@ -5,27 +5,124 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Android.Util;
+using Location.Droid.Services;
+using Android.Locations;
 
 namespace Location.Droid
 {
-    [Activity(Label = "Location.Droid", MainLauncher = true, Icon = "@drawable/icon")]
+    [Activity(Label = "LocationDroid", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
-        int count = 1;
+        readonly string logTag = "MainActivity";
+
+        // Make our labels
+        TextView latText;
+        TextView longText;
+        TextView altText;
+        TextView speedText;
+        TextView bearText;
+        TextView accText;
+
+        #region Lifecycle
 
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
+            Log.Debug(logTag, "OnCreate: Location app is becoming active");
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
-            // Get our button from the layout resource,
-            // and attach an event to it
-            Button button = FindViewById<Button>(Resource.Id.MyButton);
+            // This event fires when the ServiceConnection lets the client (our App class) know that
+            // the Service is connected. We use this event to start updating the UI with location
+            // updates from the Service
+            App.Current.LocationServiceConnected += (object sender, ServiceConnectedEventArgs e) =>
+            {
+                Log.Debug(logTag, "ServiceConnected Event raised");
+                // Notifies us of location changes from the system
+                App.Current.LocationService.LocationChanged += HandleLocationChanged;
+                // Notifies us of user changes to the location provider (i.e. the user disabled or enabled GPS)
+                App.Current.LocationService.ProviderDisabled += HandleProviderDisabled;
+                App.Current.LocationService.ProviderEnabled += HandleProviderEnabled;
+                // Notifies us of the changing status of a provider (i.e. GPS no longer available)
+                App.Current.LocationService.StatusChanged += HandleStatusChanged;
+            };
 
-            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+            latText = FindViewById<TextView>(Resource.Id.lat);
+            longText = FindViewById<TextView>(Resource.Id.longx);
+            altText = FindViewById<TextView>(Resource.Id.alt);
+            speedText = FindViewById<TextView>(Resource.Id.speed);
+            bearText = FindViewById<TextView>(Resource.Id.bear);
+            accText = FindViewById<TextView>(Resource.Id.acc);
+
+            altText.Text = "altitude";
+            speedText.Text = "speed";
+            bearText.Text = "bearing";
+            accText.Text = "accuracy";
+
+            // Start the location service:
+            App.StartLocationService();
         }
+
+        protected override void OnPause()
+        {
+            Log.Debug(logTag, "OnPause: Location app is moving to the background");
+            base.OnPause();
+        }
+
+        protected override void OnResume()
+        {
+            Log.Debug(logTag, "OnResume: Location app is moving into foreground");
+            base.OnResume();
+        }
+
+        protected override void OnDestroy()
+        {
+            Log.Debug(logTag, "OnDestroy: Location app is becoming inactive");
+            base.OnDestroy();
+
+            // Stop the location service:
+            App.StopLocationService();
+        }
+
+        #endregion
+
+        #region Android Location Service methods
+
+        public void HandleLocationChanged(object sender, LocationChangedEventArgs e)
+        {
+            Android.Locations.Location location = e.Location;
+            Log.Debug(logTag, "Foreground updating");
+
+            // These events are on a background thread, need to update on the UI thread
+            RunOnUiThread(() =>
+            {
+                latText.Text = String.Format("Latitude: {0}", location.Latitude);
+                longText.Text = String.Format("Longitude: {0}", location.Longitude);
+                altText.Text = String.Format("Altitude: {0}", location.Altitude);
+                speedText.Text = String.Format("Speed: {0}", location.Speed);
+                accText.Text = String.Format("Accuracy: {0}", location.Accuracy);
+                bearText.Text = String.Format("Bearing: {0}", location.Bearing);
+            });
+        }
+
+        public void HandleProviderDisabled(object sender, ProviderDisabledEventArgs e)
+        {
+            Log.Debug(logTag, "Location provider disabled event raised");
+        }
+
+        public void HandleProviderEnabled(object sender, ProviderEnabledEventArgs e)
+        {
+            Log.Debug(logTag, "Location provider enabled event raised");
+        }
+
+        public void HandleStatusChanged(object sender, StatusChangedEventArgs e)
+        {
+            Log.Debug(logTag, "Location status changed event raised");
+        }
+
+        #endregion
     }
 }
 
--- Location.Droid/Resources/Resource.Designer.cs
+++ Location.Droid/Resources/Resource.Designer.cs
@@ -60,8 +60,26 @@ namespace Location.Droid
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050005
+			public const int acc = 2131034117;
+			
+			// aapt resource value: 0x7f050003
+			public const int alt = 2131034115;
+			
+			// aapt resource value: 0x7f050006
+			public const int bear = 2131034118;
+			
+			// aapt resource value: 0x7f050001
+			public const int lat = 2131034113;
+			
+			// aapt resource value: 0x7f050002
+			public const int longx = 2131034114;
+			
+			// aapt resource value: 0x7f050004
+			public const int speed = 2131034116;
+			
 			// aapt resource value: 0x7f050000
-			public const int MyButton = 2131034112;
+			public const int textView1 = 2131034112;
 			
 			static Id()
 			{
